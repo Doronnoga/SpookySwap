b6a5535b55c61faa9cecb9128756705880885c1a
Doronnoga <doronnoga@gmail.com>
2024-10-04 12:24:55 +0300

    midwork button

--- Assets/Scripts/CoreGameplay/Platforms/MovingPlatformsButton.cs
+++ Assets/Scripts/CoreGameplay/Platforms/MovingPlatformsButton.cs
@@ -2,18 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MovingPlatform;
+using Unity.VisualScripting;
 
 public class MovingPlatformsButton : MovingPlatforms
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Button Controls")]
+    private bool isActivated = false;
+    [SerializeField]
+    private Collider2D buttonCollider;
+    [SerializeField]
+    protected string intendedTag = "";
+    public void ActivatePlatform()
     {
-        
+        isActivated = true;
+    }
+    public void DeactivatePlatform() 
+    {
+        isActivated = false;
     }
 
-    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other = buttonCollider;
+        Debug.Log("Enter");
+
+        if (other.CompareTag(intendedTag))
+        { 
+          ActivatePlatform();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        other = buttonCollider;
+        Debug.Log("Exit");
+        if (other == buttonCollider && other.CompareTag(intendedTag))
+        {
+            DeactivatePlatform();
+        } 
+    }
+
+    protected override void movePlatform()
+    {
+        base.movePlatform();
+    }
+
+    void Start()
+    {
+        isActivated = false;
+        if (buttonCollider == null)
+        {
+            buttonCollider = GameObject.FindGameObjectWithTag("Button").GetComponent<Collider2D>();
+        }
+    }
+
     void Update()
     {
-        
+        if (isActivated) 
+        {
+            movePlatform();
+        }
     }
 }

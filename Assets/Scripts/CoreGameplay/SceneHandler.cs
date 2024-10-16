2ed22e0639fc29e08525009f7a15bbae5f728d92
Doronnoga <doronnoga@gmail.com>
2024-10-16 19:57:36 +0300

    moving and updating scenes for scene handler

--- Assets/Scripts/CoreGameplay/SceneHandler.cs
+++ Assets/Scripts/CoreGameplay/SceneHandler.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class SceneHandler : MonoBehaviour
+namespace SceneHandlerScript
 {
-    // Start is called before the first frame update
-    void Start()
+    public class SceneHandler : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private int sceneIndex;
+        private int currentScene;
+        [SerializeField]
+        private int nextScene;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            currentScene= SceneManager.GetActiveScene().buildIndex;
+            nextScene= SceneManager.GetActiveScene().buildIndex +1;
+        }
+        public void goToStartScene()
+        {
+            Debug.Log("went to starts scene");
+            SceneManager.LoadScene(0);
+        }
+
+        public void reloadScene()
+        {
+            Debug.Log("reloaded scene");
+            SceneManager.LoadScene(currentScene);
+        }       
+
+        public void loadSpecigicLevel() 
+        {
+            Debug.Log($"went to specific scene {sceneIndex}");
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+        public void closeApplication() 
+        {
+            Debug.Log("Exit");
+            Application.Quit();
+        }
+        public void loadNextScene() 
+        {
+            Debug.Log($"went to next scene {nextScene}");
+            SceneManager.LoadScene(nextScene);
+        }
     }
 }

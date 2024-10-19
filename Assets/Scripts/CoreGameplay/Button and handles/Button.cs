0090608972b3bd3d522a44ded3314eaf818a585e
Hili64 <hilimiz23@gmail.com>
2024-10-19 19:48:01 +0300

    Lever-Button switch, button animation, scene updates
    - created a button with press animation
    - added a switch to the button prefab that would switch between the button and the lever
    - updated the scenes accordingly (except from scene 7)
    - added the updated BG to missing scenes

--- Assets/Scripts/CoreGameplay/Button and handles/Button.cs
+++ Assets/Scripts/CoreGameplay/Button and handles/Button.cs
@@ -3,34 +3,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Events;
 using UnityEngine;
+using Unity.VisualScripting;
+using UnityEngine.UIElements;
+using System.Diagnostics;
 
-namespace ButtonClass 
-{ 
-   public class Button : MonoBehaviour
+namespace ButtonClass
 {
-    [SerializeField]
-    private string tagToInteract = "Body";
-
-    public delegate void ButtonPressed(); //declare a delegate type
-
-    public event ButtonPressed OnButtonPressed; //event that reatcs to delegate
-    public event ButtonPressed OnButtonReleased;
-
-    private void OnTriggerEnter2D(Collider2D other)
+    public class Button : MonoBehaviour
     {
-        if (other.CompareTag(tagToInteract)) 
+        public ControlType controlType; // declaration of the switch
+        public GameObject buttonController;
+        public GameObject leverController;
+        private Animator anim;
+        private Rigidbody rb;
+        [SerializeField]
+        private string tagToInteract;
+
+        public delegate void ButtonPressed(); //declare a delegate type
+
+        public event ButtonPressed OnButtonPressed; //event that reatcs to delegate
+        public event ButtonPressed OnButtonReleased;
+
+        public void Start()
         {
-            OnButtonPressed?.Invoke();
+            anim = GetComponent<Animator>();
+            rb = GetComponent<Rigidbody>();
+            anim.SetBool("IsActivated", false);
         }
-    }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag(tagToInteract)) 
+        // This will be called in edit mode when you change something in the Inspector
+        private void OnValidate()
         {
-            OnButtonReleased?.Invoke();
+            // Ensure this only runs in edit mode, not during play
+            if (!Application.isPlaying)
+            {
+                SwitchControllers();
+            }
+        }
+
+        //switch between a button and a lever
+        public void SwitchControllers()
+        {
+            if (controlType == ControlType.Button)
+            {
+                buttonController.SetActive(true);
+                leverController.SetActive(false);
+                tagToInteract = "Body";
+            }
+            else if (controlType == ControlType.Lever)
+            {
+                buttonController.SetActive(false);
+                leverController.SetActive(true);
+                tagToInteract = "Skeleton";
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag(tagToInteract))
+            {
+                anim.SetBool("IsActivated", true);
+                OnButtonPressed?.Invoke();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag(tagToInteract))
+            {
+                anim.SetBool("IsActivated", false);
+                OnButtonReleased?.Invoke();
+            }
         }
     }
 }
 
-}
+public enum ControlType { Button, Lever } //the switch

2dfbd1b0ec300b223d13f038f563b5b93eac215f
Doronnoga <doronnoga@gmail.com>
2024-10-19 12:45:36 +0300

    wind tunnels

---
+++ Assets/Scripts/CoreGameplay/Platforms/WindTunnel.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ButtonClass;
+
+namespace WindTunnelScript
+{
+    public class WindTunnel : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector2 forceAmount; 
+
+        [Header("Target")]
+        [SerializeField]
+        protected string targetTag = "Ghost";
+
+        [Header("Button Controls")]
+        [SerializeField]
+        private Button button;
+
+        private Collider2D windCollider;
+        private ParticleSystem windParticles;
+
+        private bool isActivated = true;
+
+        void Start()
+        {
+            windCollider = GetComponent<Collider2D>();
+            windParticles = GetComponent<ParticleSystem>();
+
+            if (button != null)
+            {
+                button.OnButtonPressed += deactivateWind;
+                button.OnButtonReleased += activateWind;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (isActivated && collision.CompareTag(targetTag))
+            {
+                ApplyWindForce(collision);
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (isActivated && collision.CompareTag(targetTag))
+            {
+                ApplyWindForce(collision);
+            }
+        }
+
+        //private void OnTriggerExit2D(Collider2D collision)
+        //{
+        //    if (isActivated && collision.CompareTag(targetTag))
+        //    {
+        //    }
+        //}
+
+        private void ApplyWindForce(Collider2D collision)
+        {
+            Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Vector2 direction = (rb.transform.position - transform.position).normalized;
+                Vector2 force = direction * forceAmount;
+                rb.AddForce(force);
+            }
+        }
+
+        private void activateWind()
+        {
+            isActivated = true;
+            if (windParticles != null)
+            {
+                windParticles.Play();
+            }
+        }
+
+        private void deactivateWind()
+        {
+            isActivated = false;
+            if (windParticles != null)
+            {
+                windParticles.Stop(); 
+            }
+        }
+    }
+}
